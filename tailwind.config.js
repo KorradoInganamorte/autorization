4aa7a5b7218bda4afc735a78b01d33971d2121fe
Korrado Inganamorte <petristomin1812@gmail.com>
2023-08-08 11:25:19 +0300

    deleting unnecessary files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html","./src/**/*.{ts,tsx,sass}"],
+  content: ["./src/**/*.{tsx,sass}"],
   theme: {
     fontSize: {
       sm: '1.2rem',
